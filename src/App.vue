b3aad67f6a44654a4c2b5738550706eae9e8b62e
柯传佳 <kcj65197@gmail.com>
2024-10-23 15:52:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// import { reactive, toRefs, watchEffect } from 'vue'
+// import {
+//   mergeImportMap,
+//   Repl,
+//   useStore,
+//   useVueImportMap,
+//   type StoreState,
+//   type OutputModes,
+// } from '@vue/repl'
+// import MonacoEditor from '@vue/repl/monaco-editor'
+</script>
+
+<template>
+  <div>123</div>
+</template>
+
+<style scoped></style>
