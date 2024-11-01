20f503a11bb0fbea0086004a087f407a2775de37
Orange <kcj65197@gmail.com>
2024-11-01 19:41:33 +0800

    feat: temp

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,71 @@
 <script setup lang="ts">
-// import { reactive, toRefs, watchEffect } from 'vue'
-// import {
-//   mergeImportMap,
-//   Repl,
-//   useStore,
-//   useVueImportMap,
-//   type StoreState,
-//   type OutputModes,
-// } from '@vue/repl'
-// import MonacoEditor from '@vue/repl/monaco-editor'
+import { reactive, toRefs, ref, watchEffect } from 'vue'
+import {
+  mergeImportMap,
+  Repl,
+  useStore,
+  useVueImportMap,
+  type StoreState,
+} from '@vue/repl'
+import Monaco from '@vue/repl/monaco-editor'
+import welcomeCode from './template/welcome.vue?raw'
+import tsconfigCode from './template/tsconfig.json?raw'
+
+const replRef = ref<InstanceType<typeof Repl>>()
+const previewOptions = {
+  customCode: {
+    importCode: `
+      import 'element-plus/dist/index.css';
+      import 'element-plus/theme-chalk/dark/css-vars.css';
+      import ElementPlus from 'element-plus';
+    `,
+    useCode: `
+      app.use(ElementPlus);
+    `,
+  },
+}
+
+const { importMap: builtinImportMap, vueVersion } = useVueImportMap({})
+const storeState: Partial<StoreState> = toRefs(
+  reactive({
+    builtinImportMap: mergeImportMap(builtinImportMap.value, {
+      imports: {
+        // Rendering successful
+        'element-plus': '/node_modules/element-plus/dist/index.full.min.mjs',
+        'element-plus/': '/node_modules/element-plus/',
+
+        // Rendering failed
+        // 'element-plus':
+        //   'https://cdn.jsdelivr.net/npm/element-plus/dist/index.full.min.mjs',
+        // 'element-plus/': 'https://cdn.jsdelivr.net/npm/element-plus/',
+      },
+    }),
+    vueVersion,
+    template: {
+      welcomeSFC: welcomeCode,
+    },
+  }),
+)
+const store = useStore(storeState, location.hash)
+watchEffect(() => history.replaceState({}, '', store.serialize()))
+
+async function init() {
+  // const serializedState = location.hash.slice(1)
+  // if (serializedState) {
+  //   store.deserialize(serializedState)
+  // }
+}
+
+init()
 </script>
 
 <template>
-  <div>123</div>
+  <Repl
+    ref="replRef"
+    :store="store"
+    :editor="Monaco"
+    theme="dark"
+    :preview-theme="true"
+    :clear-console="false"
+  />
 </template>
-
-<style scoped></style>
---
+++ src/template/welcome.vue
@@ -0,0 +1,17 @@
+<script setup>
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import { getCurrentInstance } from 'vue'
+import { ref } from 'vue'
+import ElementPlus from 'element-plus'
+
+const instance = getCurrentInstance()
+instance.appContext.app.use(ElementPlus)
+
+const msg = ref('Hello World!')
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <el-input v-model="msg" />
+</template>
